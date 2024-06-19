762ed4674309480b8f008bc90434950e3bcd8c34
santy9617 <santy9617@gmail.com>
2024-06-19 16:58:26 -0500

    organizano las rutas pata hostinger

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import {  Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Servicios from './pages/Servicios';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/servicios" element={<Servicios />} />
-    </Routes>
+    <Router basename="/">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/servicios" element={<Servicios />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
+}
+
+function NotFound() {
+  return <h2>Page Not Found</h2>;
 }
 
 export default App;
